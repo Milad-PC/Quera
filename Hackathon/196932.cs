5ed91cacbd4e1b11b85bc443da61fe395a36fe79
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2023-10-13 15:08:35 +0000

    end game

---
+++ Hackathon/196932.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    internal class _196932
+    {
+        static void ans()
+        {
+            string myget = Console.ReadLine();
+
+            int n = int.Parse(myget.Split()[0]);
+            int k = int.Parse(myget.Split()[1]);
+            int[] chob = new int[n];
+
+            myget = Console.ReadLine();
+            for (int i = 0; i < n; i++)
+            {
+                chob[i] = int.Parse(myget.Split()[i]);
+            }
+
+            int numFrames = NumFrames(n, k, chob);
+
+            Console.WriteLine(numFrames);
+            Console.ReadLine();
+        }
+        static int NumFrames(int n, int k, int[] lengths)
+        {
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    for (int m = j + 1; m < n; m++)
+                    {
+                        int[] combo = { lengths[i], lengths[j], lengths[m] };
+                        if (combo.Count() == k)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Hackathon/196936.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    internal class _196936
+    {
+        static void ans(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+            for (int z = 0; z < t; z++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int[,] torob = new int[n, n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    string row = Console.ReadLine();
+                    for (int j = 0; j < n; j++)
+                    {
+                        torob[i, j] = int.Parse(row.Split()[j]);
+                    }
+                }
+
+                int[,] torobChe = new int[n, n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    string row = Console.ReadLine();
+                    for (int j = 0; j < n; j++)
+                    {
+                        torobChe[i, j] = int.Parse(row.Split()[j]);
+                    }
+                }
+
+                output += CanMatchTables(torob, torobChe, n) ? "YES\n" : "NO\n";
+            }
+
+
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+        static bool CanMatchTables(int[,] table1, int[,] table2, int n)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    SwapColumns(table2, i, j,n);
+                    if (TablesEqual(table1, table2, n)) return true;
+
+                    for (int x = 0; x < n; x++)
+                    {
+                        for (int z = x + 1; z < n; z++)
+                        {
+                            SwapRows(table1, x, z,n);
+                            if (TablesEqual(table1, table2, n)) return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+
+        }
+
+        // Swap two rows in the table
+        static void SwapRows(int[,] table, int row1, int row2 , int n)
+        {
+            int temp;
+            for (int i = 0; i < n; i++)
+            {
+                temp = table[row1, i];
+                table[row1, i] = table[row2, i];
+                table[row2, i] = temp;
+            }
+        }
+
+        // Swap two columns in the table
+        static void SwapColumns(int[,] table, int col1, int col2, int n)
+        {
+            int temp;
+            for (int i = 0; i < n; i++)
+            {
+                temp = table[i, col1];
+                table[i, col1] = table[i, col2];
+                table[i, col2] = temp;
+            }
+        }
+
+        // Check if two tables are equal
+        static bool TablesEqual(int[,] table1, int[,] table2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (table1[i, j] != table2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        // Copy table2 into table2Copy
+        static void CopyTable(int[,] table2, int[,] table2Copy)
+        {
+            int n = table2.GetLength(0);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    table2Copy[i, j] = table2[i, j];
+                }
+            }
+        }
+    }
+}
